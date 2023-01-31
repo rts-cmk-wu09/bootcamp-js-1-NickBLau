368858c76cd9914a3d445ff705531053c01adab7
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-01-31 13:37:55 +0100

    add quotes to array javascript.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let blockQuote= document.querySelector("blockquote")
+console.log(blockQuote);
+const quotes = [
+    ["If it isn't on Google, it doesn't exist.", "Jimmy Wales"],
+    ["It's so fine and yet so terrible to stand in front of a blank canvas.", "Paul Cezanne"],
+    ["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
+    ["Argue with idiots, and you become an idiot.", "Paul Graham"],
+    ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
+    ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"],
+    ["What do you hang on the walls of your mind?", "Eve Arnold"],
+    ["The price of greatness is responsibility.", "Winston Churchill"],
+    ["The happiest people seem to be those who have no particular cause for being happy except that they are so.", "William Inge"],
+    ["If the Sun and Moon should ever doubt, they'd immediately go out.", "William Blake"]
+  ]
+
