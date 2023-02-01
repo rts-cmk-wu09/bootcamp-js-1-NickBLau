f9bd844943179016eaaed8d27c362a2cf32e3b5a
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-02-01 09:59:29 +0100

    add Math.random to javascript

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 let blockQuote = document.querySelector("#myQuotes")
 console.log(blockQuote);
 const quotesArray = [
-    ["If it isn't on Google, it doesn't exist.", "Jimmy Wales"],
-    ["It's so fine and yet so terrible to stand in front of a blank canvas.", "Paul Cezanne"],
-    ["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
-    ["Argue with idiots, and you become an idiot.", "Paul Graham"],
-    ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
-    ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"],
-    ["What do you hang on the walls of your mind?", "Eve Arnold"],
-    ["The price of greatness is responsibility.", "Winston Churchill"],
-    ["The happiest people seem to be those who have no particular cause for being happy except that they are so.", "William Inge"],
-    ["If the Sun and Moon should ever doubt, they'd immediately go out.", "William Blake"]
+    ["If it isn't on Google, it doesn't exist.", " Jimmy Wales"],
+    ["It's so fine and yet so terrible to stand in front of a blank canvas", " Paul Cezanne"],
+    ["Good Artists Copy, Great Artists Steal", " Pablo Picasso"],
+    ["Argue with idiots, and you become an idiot", " Paul Graham"],
+    ["Be yourself; everyone else is already taken", " Oscar Wilde"],
+    ["Simplicity is the ultimate sophistication", " Leonardo Da Vinci"],
+    ["What do you hang on the walls of your mind?", " Eve Arnold"],
+    ["The price of greatness is responsibility", " Winston Churchill"],
+    ["The happiest people seem to be those who have no particular cause for being happy except that they are so", " William Inge"],
+    ["If the Sun and Moon should ever doubt, they'd immediately go out", " William Blake"]
   ]
 console.log(quotesArray);
+
+let random = Math.floor(Math.random() * quotesArray.length);
+console.log(random);
 
 // Math.random
 
@@ -24,6 +27,10 @@ console.log(quotesArray);
 
 // blockQuote.innerText = quotes.mathrandom
 
-function randomQuote() {
-  
-}
+
+
+// blockQuote.innerText = quotesArray
+
+// function randomQuote(event) {
+// let quotesArray = Math.random(1)
+// }
